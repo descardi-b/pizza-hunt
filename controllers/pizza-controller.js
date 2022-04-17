b9630bfc074601751b828a4cc695bab61be72ada
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-04-17 14:22:07 -0400

    added CRUD methods for pizza model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -29,7 +29,41 @@ const pizzaController = {
             res.status(400).json(err);
         });
     },
-    
+
+    // create pizzas
+    createPizza({ body}, res) {
+        // destructures the body out of the req object
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update pizzas by id
+    updatepizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(404).json(err));
+    },
+
+    // delete a pizza by id
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+
 }
 
 module.exports = pizzaController;
